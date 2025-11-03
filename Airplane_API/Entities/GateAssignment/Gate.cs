a95aeff044ac3d83039fce2ec31803a87a29869b
Abdelrahman H. Atta <abdelrahmanhossam2222@gmail.com>
2025-11-03 14:00:16 +0200

    feat:CreateGateAssignmentEntity

--- Airplane_API/Entities/GateAssignment/Gate.cs
+++ Airplane_API/Entities/GateAssignment/Gate.cs
@@ -1,15 +1,15 @@
-﻿using Airplane_API.Enums;
+﻿using Airplane_API.Entities.Base;
+using Airplane_API.Enums;
 
 namespace Airplane_API.Entities.GateAssignment
 {
-    public class Gate
+    public class Gate : BaseEntity
     {
         public Gate()
         {
             GateAssignments = new HashSet<GateAssignment>();
         }
 
-        public int Id { get; set; }
         public string GateNumber { get; set; }
         public GateStatus Status { get; set; }
 
---
+++ Airplane_API/Entities/GateAssignment/GateAssignment.cs
@@ -0,0 +1,17 @@
+﻿using Airplane_API.Entities.AirlineCore;
+using Airplane_API.Entities.Base;
+
+namespace Airplane_API.Entities.GateAssignment
+{
+    public class GateAssignment : BaseEntity
+    {
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+        public int FlightId { get; set; }
+        public virtual Flight Flight { get; set; }
+
+        public int GateId { get; set; }
+        public virtual Gate Gate { get; set; }
+    }
+}
