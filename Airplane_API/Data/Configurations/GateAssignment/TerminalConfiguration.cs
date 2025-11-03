5ecae0a2302ceae5f4076eafef1d6664fef63107
Abdelrahman H. Atta <abdelrahmanhossam2222@gmail.com>
2025-11-03 15:20:43 +0200

    feat:CreateTerminalConfiguration

---
+++ Airplane_API/Data/Configurations/GateAssignment/TerminalConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Airplane_API.Entities.GateAssignment;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Airplane_API.Data.Configurations.GateAssignment
+{
+    public class TerminalConfiguration : IEntityTypeConfiguration<Terminal>
+    {
+        public void Configure(EntityTypeBuilder<Terminal> builder)
+        {
+            builder.HasKey(t => t.Id);
+            builder.Property(t => t.Name).IsRequired().HasMaxLength(50);
+
+            builder.HasMany(t => t.Gates)
+                .WithOne(g => g.Terminal)
+                .HasForeignKey(g => g.TerminalId)
+                .OnDelete(DeleteBehavior.Restrict); 
+
+            builder.HasMany(t => t.BaggageClaims)
+                .WithOne(bc => bc.Terminal)
+                .HasForeignKey(bc => bc.TerminalId)
+                .OnDelete(DeleteBehavior.Restrict); 
+
+            builder.HasMany(t => t.SecurityCheckpoints)
+                .WithOne(sc => sc.Terminal)
+                .HasForeignKey(sc => sc.TerminalID)
+                .OnDelete(DeleteBehavior.Restrict); 
+
+            builder.HasMany(t => t.CustomsDesks)
+                .WithOne(cd => cd.Terminal)
+                .HasForeignKey(cd => cd.TerminalId)
+                .OnDelete(DeleteBehavior.Restrict); 
+        }
+    }
+}
