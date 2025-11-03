0421eb384c71825be2c70401d08960335e6e505f
Kholoud <kholodaliabdelkader@gmail.com>
2025-11-03 16:23:10 +0200

    CreatPassengerConfiguration

---
+++ Airplane_API/Data/Configurations/AirlineCore/PassengerConfiguration.cs
@@ -0,0 +1,54 @@
+﻿using Airplane_API.Entities.AirlineCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Airplane_API.Data.Configurations.AirlineCore
+{
+    /// <summary>
+    /// Configures the entity mapping for the Passenger entity using the Entity Framework Fluent API.
+    /// </summary>
+    public class PassengerConfiguration : IEntityTypeConfiguration<Passenger>
+    {
+        /// <summary>
+        /// Configures the database mapping for the Passenger entity.
+        /// </summary>
+        public void Configure(EntityTypeBuilder<Passenger> builder)
+        {
+            /// <summary>
+            /// Sets the primary key for the Passenger entity.
+            /// </summary>
+            builder.HasKey(p => p.Id);
+
+            /// <summary>
+            /// Configures the FirstName property as required with a maximum length of 100 characters.
+            /// </summary>
+            builder.Property(p => p.FirstName)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            /// <summary>
+            /// Configures the LastName property as required 
+            /// with a maximum length of 100 characters.
+            /// </summary>
+            builder.Property(p => p.LastName)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            /// <summary>
+            /// Configures the PassportNumber property with a maximum length of 20 characters.
+            /// </summary>
+            builder.Property(p => p.PassportNumber)
+                .HasMaxLength(20);
+
+            /// <summary>
+            /// Relationship: Passenger → FlightManifest
+            /// Configures the one-to-many relationship between Passenger and FlightManifest.
+            /// Deleting a passenger who appears on a manifest is restricted.
+            /// </summary>
+            builder.HasMany<FlightManifest>()
+                .WithOne(fm => fm.Passenger)
+                .HasForeignKey(fm => fm.PassengerId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
--- Airplane_API/Entities/AirlineCore/Passenger.cs
+++ Airplane_API/Entities/AirlineCore/Passenger.cs
@@ -1,18 +1,41 @@
 ﻿using Airplane_API.Entities.Base;
+using System.Collections.Generic;
 
 namespace Airplane_API.Entities.AirlineCore
 {
+    /// <summary>
+    /// Represents a passenger who travels on one or more flights within the system.
+    /// </summary>
     public class Passenger : BaseEntity
     {
+        /// <summary>
+        /// Initializes a new instance of the Passenger class.
+        /// The constructor initializes the FlightManifests collection 
+        /// to prevent null reference issues when adding related flight records.
+        /// </summary>
         public Passenger()
         {
             FlightManifests = new HashSet<FlightManifest>();
         }
 
+        /// <summary>
+        /// Gets or sets the first name of the passenger.
+        /// </summary>
         public string FirstName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the last name of the passenger.
+        /// </summary>
         public string LastName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the passport number of the passenger.
+        /// </summary>
         public string PassportNumber { get; set; }
 
+        /// <summary>
+        /// Gets or sets the collection of flight manifests associated with this passenger.
+        /// </summary>
         public virtual ICollection<FlightManifest> FlightManifests { get; set; }
     }
 }
