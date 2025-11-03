3736fa150e833461a2edd40574c9fad16f9e99b3
Ahmed Tarek <Ahmad_Tarek_Abdalla2096@outlook.com>
2025-11-03 22:15:34 +0200

    feat : CreateBaggageClaimDtos

---
+++ Airplane_UI/DTOs/LuggageMaintnance/BaggageClaim/CreateBaggageClaimDto.cs
@@ -0,0 +1,22 @@
+﻿using Airplane_UI.Enums;
+
+namespace Airplane_UI.DTOs.LuggageMaintnance.BaggageClaim;
+/// <summary>
+/// Data transfer object for retrieving baggage claim information,
+/// </summary>
+public class CreateBaggageClaimDto
+{
+    /// <summary>
+    /// Gets or sets the Carousel Number of the Baggage Claim.
+    /// </summary>
+    public string CarouselNumber { get; set; }
+    /// <summary>
+    /// Gets or sets the Status of the Baggage Claim.
+    /// </summary>
+    public BaggageClaimStatus Status { get; set; }
+
+    /// <summary>
+    /// The ID of the Terminal that Terminaled the Baggage Claim.
+    /// </summary>
+    public int TerminalId { get; set; }
+}
