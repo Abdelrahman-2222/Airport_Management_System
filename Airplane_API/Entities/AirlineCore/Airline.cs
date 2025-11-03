ad1912d61d3abe6832e0b6445087ffe829fb28b2
Kholoud <kholodaliabdelkader@gmail.com>
2025-11-03 13:28:26 +0200

    feat:CreateAirlineConfiguration

--- Airplane_API/Entities/AirlineCore/Airline.cs
+++ Airplane_API/Entities/AirlineCore/Airline.cs
@@ -1,7 +1,12 @@
-﻿using Airplane_API.Entities.Base;
+﻿
+using Airplane_API.Entities.Base;
 
 namespace Airplane_API.Entities.AirlineCore
 {
+    /// <summary>
+    /// Represents an airline entity within the system
+    /// and inherits Id, Name and IATA_Code from CodeBaseEntity
+    /// </summary>
     public class Airline : CodeBaseEntity
     {
         
