6cac406b554cd50b4b800b4765570b0970d6d1bf
Abdelrahman H. Atta <abdelrahmanhossam2222@gmail.com>
2025-11-03 14:10:51 +0200

    feat:CreateRunwayEntity

---
+++ Airplane_API/Entities/GateAssignment/Runway.cs
@@ -0,0 +1,17 @@
+﻿using Airplane_API.Entities.Base;
+using Airplane_API.Enums;
+
+namespace Airplane_API.Entities.GateAssignment
+{
+    public class Runway : NamedBaseEntity
+    {
+        public Runway()
+        {
+            RunwaySchedules = new HashSet<RunwaySchedule>();
+        }
+
+        public RunwayStatus Status { get; set; }
+
+        public virtual ICollection<RunwaySchedule> RunwaySchedules { get; set; }
+    }
+}
