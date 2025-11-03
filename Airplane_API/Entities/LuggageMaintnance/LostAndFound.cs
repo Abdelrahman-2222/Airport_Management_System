bfad3eb4f85573424654d23a9d6e1ca905971930
Ahmed Tarek <Ahmad_Tarek_Abdalla2096@outlook.com>
2025-11-03 13:47:51 +0200

    feat : CreateLostAndFoundEntity

---
+++ Airplane_API/Entities/LuggageMaintnance/LostAndFound.cs
@@ -0,0 +1,25 @@
+﻿using Airplane_API.Entities.Base;
+using Airplane_API.Enums;
+
+namespace Airplane_API.Entities.LuggageMaintnance;
+
+/// <summary>
+/// Represents a lost and found item with its description, date found, and current status.
+/// </summary>
+public class LostAndFound : BaseEntity
+{
+    /// <summary>
+    /// Description of the lost or found item.
+    /// </summary>
+    public string ItemDescription { get; set; }
+
+    /// <summary>
+    /// The date when the item was found.
+    /// </summary>
+    public DateTime DateFound { get; set; }
+
+    /// <summary>
+    /// The current status of the lost and found item.
+    /// </summary>
+    public LostAndFoundStatus Status { get; set; }
+}
