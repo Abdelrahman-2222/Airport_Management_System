ab7cd96b301a6ae433e9f8b7a8ebe12ad41e5b09
Ahmed Tarek <Ahmad_Tarek_Abdalla2096@outlook.com>
2025-11-03 14:54:30 +0200

    feat : CreateMaintenanceTaskEntity

---
+++ Airplane_API/Entities/LuggageMaintnance/MaintenanceTask.cs
@@ -0,0 +1,31 @@
+﻿using Airplane_API.Entities.Base;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Airplane_API.Entities.LuggageMaintnance;
+/// <summary>
+/// Represents a maintenance task with a name, description, and related maintenance logs.
+/// </summary>
+public class MaintenanceTask : NamedBaseEntity
+{
+    /// <summary>
+    /// The name of the maintenance task.
+    /// </summary>
+    public string TaskName { get; set; }
+    /// <summary>
+    /// The name property mapped to the TaskName, not stored in the database.
+    /// </summary>  
+    [NotMapped]
+    public override string Name 
+    { 
+        get => TaskName; 
+        set => TaskName = value; 
+    }
+    /// <summary>
+    /// Description of the maintenance task.
+    /// </summary>
+    public string Description { get; set; }
+    /// <summary>
+    /// Collection of maintenance logs associated with this task.
+    /// </summary>
+    public virtual ICollection<MaintenanceLog> MaintenanceLogs { get; set; }
+}
