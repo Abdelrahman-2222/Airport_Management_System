8795c996e5d8d30b8e79b1452ec982a76eae2385
Kholoud <kholodaliabdelkader@gmail.com>
2025-11-04 09:42:03 +0200

    feat: Add AirportDTOs

---
+++ Airplane_API/DTOs/AirlineCore/AirportDTOs/AirportDTOs.cs
@@ -0,0 +1,23 @@
+﻿namespace Airplane_API.DTOs.AirlineCore.AirportDTOs
+{
+    /// <summary>
+    /// Represents a Data Transfer Object (DTO) for an airport.
+    /// </summary>
+    public class AirportDTOs
+    {
+        /// <summary>
+        /// Gets or sets the unique identifier of the airport.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the airport.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the International Air Transport Association (IATA) airport code.
+        /// </summary>
+        public string IATA_Code { get; set; }
+    }
+}
