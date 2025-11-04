cdd6d7a5feade4920f80ff7c14a19644fe649815
Kholoud <kholodaliabdelkader@gmail.com>
2025-11-04 16:48:56 +0200

    feat: Create Airline DTOs

---
+++ Airplane_UI/DTOs/AirlineCore/AirlineDTOs/CreateAndUpdateAirlineDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace Airplane_UI.DTOs.AirlineCore.AirlineDTOs
+{
+    /// <summary>
+    /// Represents a (DTO) used when creating a new airline record.
+    /// </summary>
+    public class CreateAndUpdateAirlineDTO
+    {
+        /// <summary>
+        /// Gets or sets the name of the airline.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the International Air Transport Association (IATA) code of the airline.
+        /// </summary>
+        public string IATA_Code { get; set; }
+    }
+}
