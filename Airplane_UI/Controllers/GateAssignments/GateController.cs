75983e511b082dd50d21b2aae6812526dc268f5d
Abdelrahman H. Atta <abdelrahmanhossam2222@gmail.com>
2025-11-04 14:20:12 +0200

    feat:CreateGateController

---
+++ Airplane_UI/Controllers/GateAssignments/GateController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Airplane_UI.Controllers.GateAssignments
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GateController : ControllerBase
+    {
+    }
+}
