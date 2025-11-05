8ec0ddbe5b73aa9cde3988017eae8603939dac07
Abdelrahman H. Atta <abdelrahmanhossam2222@gmail.com>
2025-11-05 06:32:52 +0200

    feat/CreateTerminalContract

---
+++ Airplane_UI/Contracts/GateAssignments/ITerminalService.cs
@@ -0,0 +1,22 @@
+﻿
+using Airplane_UI.DTOs.GateAssignments.TerminalDTOs;
+
+namespace Airplane_UI.Contracts.GateAssignments
+{
+    public interface ITerminalService
+    {
+        // CRUD operations for Gate entity
+        // GETALL
+        Task<IList<GetTerminalDTO>> GetAllAsync();
+        // GET All Details
+        Task<IList<GetAllDetailsTerminalDTO>> GetDetailsAsync();
+        // GET by Id
+        Task<GetTerminalDTO> GetByIdAsync(int terminalId);
+        // CREATE
+        Task<GetAllDetailsTerminalDTO> CreateAsync(CreateAndUpdateTerminalDTO terminalDto);
+        // UPDATE
+        Task<GetTerminalDTO> UpdateAsync(int terminalId, CreateAndUpdateTerminalDTO terminalDto);
+        // DELETE
+        Task<string> DeleteAsync(int terminalId);
+    }
+}
