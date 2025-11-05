93a5c4f692db8cf07707a9ff7d837bedf373c143
Kholoud <kholodaliabdelkader@gmail.com>
2025-11-05 11:46:20 +0200

    feat: creat flight manifest dtos

--- Airplane_UI/DTOs/AirlineCore/FlightManifestDTOS/GetFlightManifestDTO.cs
+++ Airplane_UI/DTOs/AirlineCore/FlightManifestDTOS/GetFlightManifestDTO.cs
@@ -1,11 +1,11 @@
-﻿using Airplane_UI.Entities.Base;
+﻿using Airplane_UI.Entities.AirlineCore;
 
-namespace Airplane_UI.Entities.AirlineCore
+namespace Airplane_UI.DTOs.AirlineCore.FlightManifestDTOS
 {
     /// <summary>
-    /// Represents a record that links a Passenger to a specific Flight.
+    /// Represents a (DTO) for retrieving flight manifest details.
     /// </summary>
-    public class FlightManifest : IBaseEntity
+    public class GetFlightManifestDTO
     {
         /// <summary>
         /// Gets or sets the unique identifier of the flight manifest record.
@@ -23,20 +23,8 @@ namespace Airplane_UI.Entities.AirlineCore
         public int FlightId { get; set; }
 
         /// <summary>
-        /// Gets or sets the related Flight entity.
-        /// Represents the flight on which the passenger is booked.
-        /// </summary>
-        public virtual Flight Flight { get; set; }
-
-        /// <summary>
         /// Gets or sets the unique identifier of the passenger associated with this manifest entry.
         /// </summary>
         public int PassengerId { get; set; }
-
-        /// <summary>
-        /// Gets or sets the related Passenger entity.
-        /// Represents the passenger recorded on the flight manifest.
-        /// </summary>
-        public virtual Passenger Passenger { get; set; }
     }
 }
