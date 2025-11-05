7d0a31a7d9e271a18c669d7df27bb29903c26954
Abdelrahman H. Atta <abdelrahmanhossam2222@gmail.com>
2025-11-05 07:47:13 +0200

    feat/CreateRunwayContract

---
+++ Airplane_UI/DTOs/GateAssignments/RunwayDTOs/GetAllDetailsRunwayDTO.cs
@@ -0,0 +1,16 @@
+﻿using Airplane_UI.DTOs.GateAssignments.RunwayScheduleDTOs;
+using Airplane_UI.Entities.GateAssignments;
+using Airplane_UI.Enums;
+
+namespace Airplane_UI.DTOs.GateAssignments.RunwayDTOs
+{
+    public class GetAllDetailsRunwayDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Status { get; set; }
+
+        public virtual ICollection<GetRunwayScheduleDTO> RunwaySchedules { get; set; } = new HashSet<GetRunwayScheduleDTO>();
+    }
+}
