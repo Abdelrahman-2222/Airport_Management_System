3c390b83c8aabe185c70b0265405d77bc46174d1
Ahmed Tarek <Ahmad_Tarek_Abdalla2096@outlook.com>
2025-11-06 07:17:17 +0200

    feat : addBaggageClaimBlazor

---
+++ Airplane_UI/Components/Pages/BaggageClaim.razor.cs
@@ -0,0 +1,37 @@
+﻿using Airplane_UI.DTOs.LuggageMaintnance.BaggageClaim;
+
+namespace Airplane_UI.Components.Pages;
+
+public partial class BaggageClaim
+{
+    private IList<GetBaggageClaimDto> baggageClaims = new List<GetBaggageClaimDto>();
+    private bool _loading = false;
+    private string _errorMessage = string.Empty;
+
+    protected override async Task OnInitializedAsync()
+    {
+        await LoadBaggageClaimsAsync();
+    }
+
+    private async Task LoadBaggageClaimsAsync()
+    {
+        try
+        {
+            _loading = true;
+            _errorMessage = string.Empty;
+            baggageClaims = await BaggageClaimService.GetAllAsync();
+        }
+        catch (Exception ex)
+        {
+            _errorMessage = $"Error loading baggage claim: {ex.Message}";
+        }
+        finally
+        {
+            _loading = false;
+        }
+    }
+    private void NavigateToDetails(int baggagcelaimId)
+    {
+        Navigation.NavigateTo($"/baggagcelaim/details/{baggagcelaimId}");
+    }
+}
