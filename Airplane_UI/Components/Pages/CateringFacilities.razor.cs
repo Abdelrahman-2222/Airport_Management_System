29f5b1de9a39ab0b117fe3dba679104be54807e8
Ahmed Tarek <Ahmad_Tarek_Abdalla2096@outlook.com>
2025-11-06 08:02:04 +0200

    feat : CreateCateringFacilitiesBlazor

---
+++ Airplane_UI/Components/Pages/CateringFacilities.razor.cs
@@ -0,0 +1,39 @@
+﻿using Airplane_UI.DTOs.LuggageMaintnance.CateringFacilitiesDTOs;
+using Airplane_UI.Services.AirlineCore;
+
+namespace Airplane_UI.Components.Pages
+{
+    public partial class CateringFacilities
+    {
+        private IList<GetCateringFacilitiesDTO> Facilities = new List<GetCateringFacilitiesDTO>();
+        private bool _loading = false;
+        private string _errorMessage = string.Empty;
+
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadAirlinesAsync();
+        }
+
+        private async Task LoadAirlinesAsync()
+        {
+            try
+            {
+                _loading = true;
+                _errorMessage = string.Empty;
+                Facilities = await CateringFacilitiesService.GetAllAsync();
+            }
+            catch (Exception ex)
+            {
+                _errorMessage = $"Error loading cateringfacilities: {ex.Message}";
+            }
+            finally
+            {
+                _loading = false;
+            }
+        }
+        private void NavigateToDetails(int airlineId)
+        {
+            Navigation.NavigateTo($"/cateringfacilities/details/{airlineId}");
+        }
+    }
+}
---
+++ Airplane_UI/Components/Pages/CateringFacilitiesCreate.razor.cs
@@ -0,0 +1,40 @@
+﻿using Airplane_UI.DTOs.LuggageMaintnance.CateringFacilitiesDTOs;
+
+namespace Airplane_UI.Components.Pages
+{
+    public partial class CateringFacilitiesCreate
+    {
+        private CreateAndUpdateCateringFacilitiesDTO facilitiesModel = new();
+
+        private bool isSaving = false;
+        private string errorMessage = string.Empty;
+
+        private async Task HandleCreateAsync()
+        {
+            isSaving = true;
+            errorMessage = string.Empty;
+
+            try
+            {
+                await CateringFacilitiesService.CreateAsync(facilitiesModel);
+
+                Navigation.NavigateTo("/cateringfacilities");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error creating cateringfacilities: {ex.Message}");
+
+                errorMessage = "An error occurred while creating the cateringfacilities. Please try again.";
+            }
+            finally
+            {
+                isSaving = false;
+            }
+        }
+
+        private void GoBack()
+        {
+            Navigation.NavigateTo("/cateringfacilities");
+        }
+    }
+}
